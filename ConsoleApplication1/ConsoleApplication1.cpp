3fb47f0eb322b7e34e5cef2a5ca45ed006fa475e
danielelectronicpayments <120591889+danielelectronicpayments@users.noreply.github.com>
2023-05-12 15:45:02 -0400

    Attempting to resolve the issue with case 3

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,3 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <cmath>
 #include <ctime>
@@ -20,56 +17,77 @@ int main()
     int number;
     std::string myResponse = "Y";
     std::cout << "My first C++ application! \n";
-    std::time_t now = std::time(nullptr);
-    char timeString[26];
-    struct tm localTime;
-    localtime_s(&localTime, &now);
-    std::strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &localTime);
-    std::cout << "The current time is: " << timeString << "\n\n" ;
-    
-
+    int choice;
     Person p;
-    std::cout << "Enter name: ";
-    std::getline(std::cin, p.name);
-    std::cout << "Enter your age: ";
-    while (!(std::cin >> p.age)) {
-        std::cout << "Invalid input. Please enter a number for your age: ";
-        std::cin.clear(); // Clear the error state
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the remaining input in the input buffer.
-    }
- 
-    std::cout << "Enter your hair color: ";
-    std::cin.ignore(); // ignore the newline character left in the input stream after reading p.age
-    std::getline(std::cin, p.hair);
+    std::string now;
+    do {
+    
+     std::cout << "What would you like to do? \n 1. Introduction \n 2. Current Time \n 3. Numbers \n";
+     std::cin >> choice;
+     std::cin.ignore(); // ignore the newline character left in the input stream after reading choice
 
-    std::cout << "Enter your eye color: ";
-    //std::cin.ignore(); // ignore the newline character left in the input stream after reading p.age
-    std::getline(std::cin, p.eyes);
+     switch (choice) {
 
-    std::cout << "Hello " << p.name << " \n";
-    std::cout << "Wow, you are " << p.age << " years old! \n";
-    std::cout << "I always liked the combination of " << p.hair << " hair and " << p.eyes << " eyes \n\n" ;
+     case 1:
+         std::cout << "Enter name: ";
+         std::getline(std::cin, p.name);
+         std::cout << "Enter your age: ";
+         while (!(std::cin >> p.age)) {
+             std::cout << "Invalid input. Please enter a number for your age: ";
+             std::cin.clear(); // Clear the error state
+             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the remaining input in the input buffer.
+         }
+
+         std::cout << "Enter your hair color: ";
+         std::cin.ignore(); // ignore the newline character left in the input stream after reading p.age
+         std::getline(std::cin, p.hair);
+
+         std::cout << "Enter your eye color: ";
+         std::getline(std::cin, p.eyes);
+
+         std::cout << "Hello " << p.name << " \n";
+         std::cout << "Wow, you are " << p.age << " years old! \n";
+         std::cout << "I always liked the combination of " << p.hair << " hair and " << p.eyes << " eyes \n\n";
+         break;
+
+     case 2:
 
 
-  do{
-        std::cout << "Enter a number between 1 and 9\n";
-        while (!(std::cin >> number)) {
-            std::cout << "Invalid input. Please enter a valid number: ";
-            std::cin.clear(); // Clear the error state
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the remaining input in the input buffer.
-        }
-        std::cout << "You entered: " << number << "\n";
-        int double_number = number * 2;
-        std::cout << "Your number doubled would be " << double_number << "\n";
-        std::cout << "Would you like to try again? (Enter 'Yes' or 'No')\n";
-        std::cin >> myResponse;
+         std::time_t now = std::time(nullptr);
+         char timeString[26];
+         struct tm localTime;
+         localtime_s(&localTime, &now);
+         std::strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &localTime);
+         std::cout << "The current time is: " << timeString << "\n\n";
+         break;
+
+
+      /*
+     case 3:
+         std::cout << "Enter a number between 1 and 9\n";
+         while (!(std::cin >> number)) {
+             std::cout << "Invalid input. Please enter a valid number: ";
+             std::cin.clear(); // Clear the error state
+             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the remaining input in the input buffer.
+         }
+         std::cout << "You entered: " << number << "\n";
+         int double_number = number * 2;
+         std::cout << "Your number doubled would be " << double_number << "\n";
+         break; */
+     }
+
+     std::cout << "Would you like to try again? (Enter 'Yes' or 'No')\n";
+     std::cin >> myResponse;
+
+    
+        
+        
+        
+   
+
   } while (myResponse == "Y" || myResponse == "y" || myResponse == "Yes" || myResponse == "yes" || myResponse == "YES");
-        
-        
-        
-        return 0;
 
-
+  return 0;
 
 }
 
