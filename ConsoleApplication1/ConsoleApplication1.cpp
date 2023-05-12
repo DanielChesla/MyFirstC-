4311024d1e3b156b77bcea0bcb216196d856afd9
danielelectronicpayments <120591889+danielelectronicpayments@users.noreply.github.com>
2023-05-12 10:33:27 -0400

    Adding do-while loop

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,15 +6,23 @@
 int main()
 {
     int number;
-    
+    char myResponse = 'Y';
     std::cout << "My first C++ application!\n\n\n";
     std::cout << "Adding a second line!\n\n\n";
-    std::cout << "Enter a number between 1 and 9\n";
-    std::cin >> number;
-    std::cout << "You entered: " << number << "\n";
-    int double_number = number * 2;
-    std::cout << "Your number doubled would be " << double_number << "\n";
-    return 0;
+    
+  do{
+        std::cout << "Enter a number between 1 and 9\n";
+        std::cin >> number;
+        std::cout << "You entered: " << number << "\n";
+        int double_number = number * 2;
+        std::cout << "Your number doubled would be " << double_number << "\n";
+        std::cout << "Would you like to try again?\n";
+        std::cin >> myResponse;
+    }  while(myResponse == 'Y');
+        
+        
+        
+        return 0;
 
 
 
